b9b8116e5b0f5ff347e9bb9d9834dc659bac598a
Matej Habrnal <mhabrnal@redhat.com>
2015-03-19 17:33:30 +0100

    adds delete problems functionality
    Signed-off-by: Matej Habrnal <mhabrnal@redhat.com>

--- abrt/abrt.js
+++ abrt/abrt.js
@@ -1,6 +1,7 @@
 $( document ).ready( function() {
     var required_elements = ["time", "reason", "package", "container_image", "container_id", "count"];
     var detail_elements = ["reason", "backtrace", "cmdline", "executable", "package", "component", "pid", "hostname", "count", "first_occurence", "last_occurence", "user", "type", "duphash", "os_release", "abrt_version", "runlevel", "kernel", "architecture", "uuid", "ureports_counter", "data_directory", "reported_to", "os_info", "environ"];
+    var dropdown_elements = ["backtrace", "", ""]
 
     var service = cockpit.dbus('org.freedesktop.problems');
     var problems = service.proxy('org.freedesktop.problems', '/org/freedesktop/problems');
@@ -26,7 +27,9 @@ $( document ).ready( function() {
             row += "<td>" + format_problem_data(problem_data, elem) + "</td>";
         });
 
-        return "<tr id=\"" + problem_id + "\" class=\"problem\">" + row + "</tr><tr class=\"hidden\"><td colspan=\"6\"></td></tr>";
+        row += get_action_btn();
+
+        return "<tr id=\"" + problem_id + "\" class=\"problem\">" + row + "</tr><tr class=\"detail_list hidden\"><td colspan=\"7\"></td></tr>";
     }
 
     function format_problem_data(problem_data, elem) {
@@ -43,31 +46,87 @@ $( document ).ready( function() {
         return value;
     }
 
-    $( document ).on('click', '.problem', function() {
+    function get_action_btn() {
+        var btn = "<td class=\"action_btn\">";
+        btn += "<div class=\"btn-group\">";
+        btn += "<button class=\"btn btn-default main-btn\" title=\"\">";
+        btn += "Delete";
 
-        var detail_row = $(this).next();
+/* Prepared for adding another functionality
+ *
+        btn += "</button>";
+        btn += "<button class=\"btn btn-default dropdown-toggle\" data-toggle=\"dropdown\">";
+        btn += "<span class=\"caret\"></span>";
+        btn += "</button>";
+
+        btn += "<ul class=\"dropdown-menu\" role=\"menu\">";
+        btn += get_btn_dropdown_li("Report");
+        btn += get_btn_dropdown_li("Detail");
+        btn += "</ul>";
+*/
+
+        btn += "</div>";
+        btn += "</td>";
+
+        return btn;
+    }
+
+    function get_btn_dropdown_li(label) {
+        var li = "<li class=\"presentation\">";
+        li += "<a role=\"menuitem\">";
+        li += "<span>" + label + "</span>";
+        li += "</a>";
+        li += "</li>";
+
+        return li;
+    }
+
+    $( document ).on('click', '.problem', function() {
+        problem_detail(this);
+    });
+
+    $( document ).on('click', '.detail_list', function() {
+        problem_detail($(this).prev());
+    });
+
+    $( document ).on('click', '.main-btn', function( event ) {
+        event.stopPropagation();
+        var problem = $(this).closest('tr');
+        var problem_id = $(problem).attr('id');
+        console.log(problem_id);
+        var del = problems.DeleteProblem([problem_id])
+            del.done(function() {
+                //console.log(problem_id + " deleted.");
+                $(problem).addClass("hidden");
+                $(problem).next().addClass("hidden");
+            });
+
+    });
+
+    function problem_detail( problem ) {
+        var detail_row = $(problem).next();
 
         /* detail is shown */
-        if ($(this).hasClass("selected_problem")) {
+        if ($(problem).hasClass("selected_problem")) {
 
-            $(this).removeClass("selected_problem");
+            $(problem).removeClass("selected_problem");
             $(detail_row).addClass("hidden");
         }
         /* detail is not displayed, but it is loaded */
         else if ($(detail_row).hasClass("loaded")) {
-            $(this).addClass("selected_problem");
+            $(problem).addClass("selected_problem");
             $(detail_row).removeClass("hidden");
         }
         /* detail is not displayed nor loaded */
         else {
-            create_detail_table(this);
+            create_detail_table(problem);
 
-            $(this).addClass("selected_problem");
+            $(problem).addClass("selected_problem");
             $(detail_row)
                 .removeClass("hidden")
                 .addClass("loaded");
         }
-    });
+    }
 
     function create_detail_table(row) {
         var problem_id = $(row).attr('id');
@@ -77,7 +136,6 @@ $( document ).ready( function() {
 
                 var result = create_detail(problem_data, problem_id);
 
-                //$(result).insertAfter(row);
                 $(row).next().children().append(result);
             });
     }
@@ -89,15 +147,14 @@ $( document ).ready( function() {
             if(problem_data.hasOwnProperty(elem)) {
 
                 var problem_content = problem_data[elem][2];
+
                 problem_content = problem_content.replace(/\n/g, "<br>");
                 /* bold variable */
-                problem_content = problem_content.replace(/([A-Z_]+=)/g, "<b>$1</b>");
+                problem_content = problem_content.replace(/(<br>[^=]+=|^[^=]+=)/g, "<b>$1</b>");
 
                 text += "<tr class=\"detail\"><td class=\"detail_label\">" + elem + "</td><td class=\"detail_content\">" + problem_content + "</td></tr>";
             }
         }
         return text;
     }
-
-
 });
